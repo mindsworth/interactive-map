dfd03e6911a010bf5e1ce6e5325e1685df2d6138
Prince Goziem <princegoziem@gmail.com>
2019-06-15 10:56:18 +0100

    customized map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function App() {
+const App = () => {
+	const [mapViewport, setViewport] = useState({
+		width: '100vw',
+		height: '60vh',
+		latitude: 6.45407,
+		longitude: 3.39467,
+		zoom: 12
+	});
+
+	console.log(process.env.REACT_APP_MAPBOX_TOKEN, 'Hello====>>>');
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1>Map-BOx</h1>
+				<ReactMapGL
+					{ ...mapViewport }
+					mapStyle="mapbox://styles/princegoziem/cjwxceq2z36xh1cqyaqk99693"
+					mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+					onViewportChange={viewport => setViewport({ ...mapViewport, ...viewport })}
+				>
+					markers here
+				</ReactMapGL>
 			</header>
 		</div>
 	);
-}
+};
 
 export default App;
